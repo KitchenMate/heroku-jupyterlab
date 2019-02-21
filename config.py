c1919a2f43234f42ec8861d5f5ce3afe9a55d9c4
Lucas Zeer <lucas.zeer@gmail.com>
2019-02-21 16:23:12 -0500

    initalize on launch

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ c.NotebookApp.password = 'sha1:082abc44d5f3:d85dfd9b5c053b6a605ba757c40e0e5c3e41
 
 c.LabApp.trust_xheaders = True
 # print("Starting Nginx!")
-# Path('/tmp/app-initialized').touch()
+Path('/tmp/app-initialized').touch()
 
 
 # LabApp.ssl_options=<Dict>
